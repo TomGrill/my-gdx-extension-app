5c899a80eafabb84db0775403775340d38e0f5f5
Thomas Pronold <tp@tpronold.de>
2016-07-16 19:25:39 +0200

    initial commit

---
+++ android/src/de/tomgrill/mygdxextension/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package de.tomgrill.mygdxextension;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import de.tomgrill.mygdxextension.MyGdxExtension;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxExtension(), config);
+	}
+}
---
+++ desktop/src/de/tomgrill/mygdxextension/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.tomgrill.mygdxextension.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.tomgrill.mygdxextension.MyGdxExtension;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxExtension(), config);
+	}
+}
